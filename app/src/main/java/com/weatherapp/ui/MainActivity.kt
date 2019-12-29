4509fdf427c5468325f33d96f178cf5d94a65146
Mudassar Hussain <junto06@gmail.com>
2019-12-30 01:27:22 +0800

    Added back button

--- app/src/main/java/com/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/weatherapp/ui/MainActivity.kt
@@ -2,12 +2,27 @@ package com.weatherapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
 import com.weatherapp.R
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //setup back button
+        val navController = findNavController(R.id.mainNavFragment)
+        appBarConfiguration = AppBarConfiguration.Builder(navController.graph).build()
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.mainNavFragment).navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
